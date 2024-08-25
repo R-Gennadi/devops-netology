77793ac65ae13eb6573952a807e873158d2c0e6b
Gennadiy R <repyevskiy.gennadiy@otr.ru>
2024-08-25 13:33:05 +0300

    modification2

---
+++ 15-clopro/Security-in-cloud/file/scr/terraform/variables.tf
@@ -0,0 +1,26 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g8dolaql3are1tu770"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gtheioau4s71j2mu0u"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd87bh3i9m7udk0gpn0u"
+}
+
+variable "zone" {
+  default = "ru-central1-a"  
+}
+
+variable "yandex_cloud_auth" {
+  default = "..."
+  sensitive = true
+}
